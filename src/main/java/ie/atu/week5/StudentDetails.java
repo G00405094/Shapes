78583e676a52a335faaa6651bb772835511ec9ec
G00405094 <G00405094@atu.ie>
2022-10-18 14:35:54 +0100

    Added New File Called Student Details

---
+++ src/main/java/ie/atu/week5/StudentDetails.java
@@ -0,0 +1,31 @@
+package ie.atu.week5;
+
+public class StudentDetails {
+    private double StuName;
+    private double StuEmail;
+    private double StuCourse;
+
+    public double getStuName() {
+        return StuName;
+    }
+
+    public void setStuName(double stuName) {
+        StuName = stuName;
+    }
+
+    public double getStuEmail() {
+        return StuEmail;
+    }
+
+    public void setStuEmail(double stuEmail) {
+        StuEmail = stuEmail;
+    }
+
+    public double getStuCourse() {
+        return StuCourse;
+    }
+
+    public void setStuCourse(double stuCourse) {
+        StuCourse = stuCourse;
+    }
+}
