f7864fe75564610214844e5030de86855c805880
G00405094 <G00405094@atu.ie>
2022-10-18 14:56:11 +0100

    Added New File Called Student Details

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -8,5 +8,14 @@ public class StudentApp {
         Scanner input = new Scanner(System.in);
         String StuName = input.nextLine();
         System.out.println("You entered: " + StuName);
+
+        System.out.println("Please enter your Student Email:");
+        String StuEmail = input.nextLine();
+        System.out.println("You entered: " + StuEmail);
+
+        System.out.println("Please enter your Course:");
+        String StuCourse = input.nextLine();
+        System.out.println("You entered: " + StuCourse);
+
     }
 }
